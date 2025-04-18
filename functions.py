0dc7c38de322345fc3e4c64ed9e59307b5fea436
DocKlaus <doct.klaus@gmail.com>
2025-04-18 18:24:53 +0300

    Разделы, конфиги, сортировка.
    Добавлены дополнительные разделы под каждый процесс. Пока установлены по умолчанию внутри кода. Добавлен документ с конфигами, которые может настроить пользователь под себя. Улучшена система обновления информации и сортировки в итоговых документах.

--- functions.py
+++ functions.py
@@ -10,6 +10,8 @@ import win32process
 import pyautogui
 import psutil
 
+import pprint
+
 
 # Глобальные переменные для отслеживания активности
 current_window = None  # Текущее активное окно
@@ -24,6 +26,58 @@ end_afk = 0                      # Время окончания AFK
 
 today = time.time()  # Текущее время для форматирования даты
 
+# Ориентация по разделам
+Sections = {
+    'Steam': ['steam'],
+    'Браузер': ['google', 'chrome', 'yandex'],
+    'Мессенджеры': ['discord', 'telegram'],
+    'Работа': ['code']
+}
+
+def get_config_info(config_file):
+    try:
+        with open(config_file, 'r', encoding = 'utf-8') as file:
+            lines = file.readlines()
+            line_iterator = iter(lines)
+            time_check_sec = None
+            time_afk = None
+            for line in line_iterator:
+                line = line.strip()
+                if 'time_check_sec' in line:
+                    time_check_sec = line.split(' = ')[1].strip()                    
+                if 'time_afk' in line:
+                    time_afk = line.split(' = ')[1].strip()
+                if time_check_sec and time_afk != None:
+                    break
+            with open(f'activity_report_{custom_format()}.txt', 'a', encoding = 'utf-8') as file:
+                if time_check_sec == None:
+                    time_check_sec = 5
+                    log = f'Параметр time_check_sec (частота опроса) установлен по умолчанию = {time_check_sec} сек.'
+                else:
+                    log = f'Параметр time_check_sec (частота опроса) установлен пользователем = {time_check_sec} сек.'
+                
+                file.write(f'{log}\n')
+                print(f'{log}')
+
+                if time_afk == None:
+                    time_afk = 3
+                    log = f'Параметр time_afk (фиксирование простоя) установлен по умолчанию = {time_afk} мин.'
+                else:
+                    log = f'Параметр time_afk (фиксирование простоя) установлен пользователем = {time_afk} мин.'
+                
+                file.write(f'{log}\n')
+                print(f'{log}')
+       
+        return {
+            'time_check_sec': int(time_check_sec),
+            'time_afk': int(time_afk)
+            }
+    
+    except FileNotFoundError:
+        with open(f'Отчёт об ошибках {custom_format()}', 'a', encoding = 'utf-8') as file:
+            file.write(f'Файл {config_file} не найден')
+    except UnicodeDecodeError:
+        print("Возможно, файл имеет другую кодировку")
 
 # Получение информации об активном окне
 def get_active_window_info():
@@ -34,13 +88,21 @@ def get_active_window_info():
     # Получаем PID процесса
     _, pid = win32process.GetWindowThreadProcessId(window_handle)
     # Получаем информацию о процессе
-    process = psutil.Process(pid)
-    process_name = process.name()
-    process_path = process.exe()
-    
+    try:
+        process = psutil.Process(pid)
+        process_name = process.name()
+        process_path = process.exe()
+
+    except psutil.NoSuchProcess: 
+        with open(f'Отчёт об ошибках {custom_format()}', 'a', encoding = 'utf-8') as file:
+            file.write(f'process PID not found (pid={pid}) \n File "c:\\projects\\GIT\\Tracker\\functions.py"')
+        process = 'Не найден. Создан отчёт об ошибке'
+        process_name = 'Неизвестно'
+        process_path = 'Неизвестно'
     return {
         'window_title': window_title,
         'process_name': process_name,
+        'process_path': process_path
     }
 
 
@@ -70,21 +132,23 @@ def afk_counter():
 
 
 # Проверка состояния AFK
-def checking_afk():
+def checking_afk(config_info):
     global afk_count, start_afk
-    if afk_count > 3:  # Если бездействие более 3 проверок
-        start_afk = time.time() - afk_count
+    time_afk = config_info['time_afk']
+    time_check_sec = config_info['time_check_sec']
+    if afk_count > time_afk * 60 / time_check_sec:  
+        start_afk = time.time() - afk_count * 60 / time_check_sec
         return True
     return False
     
 
 # Определение возобновления активности
-def resumption_activity():
+def resumption_activity(config_info):
     global end_afk, start_afk
-    fixed_afk = checking_afk()
+    fixed_afk = checking_afk(config_info)
     afk_counter()
     
-    if not checking_afk() and fixed_afk:  # Если активность возобновилась
+    if not checking_afk(config_info) and fixed_afk:  # Если активность возобновилась
         end_afk = time.time()
         return start_afk, end_afk
     else:
@@ -119,46 +183,113 @@ def create_time_based_report(start_time, end_time, current_window):
             file.write(f'{format_time(start_time)}-{format_time(end_time)} : Desktop\n')
 
 
-# Создание словаря с имеющимися процессами и окнами
-def create_process_dict(process_name, window_name, start_time, end_time):
-    global process_dict # Используем глобальную переменную для хранения данных
-    duration = end_time - start_time # Вычисляем длительность активности
+# Функция для создания структуры словаря
+def create_process_dict(process_name, window_name, process_path, start_time, end_time):
+    global process_dict
+    duration = end_time - start_time
+    
+    section = 'Other'
+    for subsection in Sections:
+        for name in Sections[subsection]:
+            if name.lower() in process_path.lower():
+                section = subsection
+                break
+    
+    # Создаём структуру по секциям, если её нет
+    if section not in process_dict:
+        process_dict[section] = {
+            'total_duration': 0,
+            'processes': {}
+        }
     
     # Если процесс еще не существует в словаре
-    if process_name not in process_dict:
-        process_dict[process_name] = {
-            'windows': {}, # Словарь для хранения окон процесса
-            'total_duration': 0 # Общая длительность работы процесса
+    if process_name not in process_dict[section]['processes']:
+        process_dict[section]['processes'][process_name] = {
+            'windows': {},
+            'total_duration': 0
         }
     
     # Обработка информации об окнах
-    if window_name not in process_dict[process_name]['windows']:
-        # Если окно новое, создаем его запись
-        process_dict[process_name]['windows'][window_name] = {
-            'duration': duration # Длительность работы окна
+    if window_name not in process_dict[section]['processes'][process_name]['windows']:
+        if window_name == '' or None:
+            process_dict[section]['processes'][process_name]['windows']['Desktop'] = {
+            'duration': duration
         }
+        else:
+            process_dict[section]['processes'][process_name]['windows'][window_name] = {
+                'duration': duration
+            }
     else:
-        # Если окно уже существует, добавляем длительность
-        process_dict[process_name]['windows'][window_name]['duration'] += duration
+        process_dict[section]['processes'][process_name]['windows'][window_name]['duration'] += duration
     
-    # Обновляем общую длительность процесса
-    process_dict[process_name]['total_duration'] += duration
+    # Обновляем длительности
+    process_dict[section]['processes'][process_name]['total_duration'] += duration
+    process_dict[section]['total_duration'] += duration
+
+# Функция для сортировки секций по длительности
+def sort_sections_by_duration():
+    global process_dict
+    sorted_dict = {}
+    for section, data in sorted(process_dict.items(), key=lambda x: x[1]['total_duration'], reverse=True):
+        sorted_dict[section] = {
+            'total_duration': data['total_duration'],
+            'processes': sort_processes_by_duration(data['processes'])
+        }
+    process_dict = sorted_dict
+
+# Функция для сортировки процессов внутри секции
+def sort_processes_by_duration(processes):
+    sorted_processes = {}
+    for process, data in sorted(processes.items(), key=lambda x: x[1]['total_duration'], reverse=True):
+        sorted_processes[process] = {
+            'total_duration': data['total_duration'],
+            'windows': sort_windows_by_duration(data['windows'])
+        }
+    return sorted_processes
+
+# Функция для сортировки окон внутри процесса
+def sort_windows_by_duration(windows):
+    return {
+        window: {'duration': data['duration']} 
+        for window, data in sorted(windows.items(), key=lambda x: x[1]['duration'], reverse=True)
+    }
+
+# Функция для полной сортировки всего словаря
+def sort_all_by_duration():
+    sort_sections_by_duration()
+    for section in process_dict:
+        process_dict[section]['processes'] = sort_processes_by_duration(process_dict[section]['processes'])
+        for process in process_dict[section]['processes']:
+            process_dict[section]['processes'][process]['windows'] = sort_windows_by_duration(process_dict[section]['processes'][process]['windows'])
+
 
 
 # Функция для сохранения словаря в txt-файл
-def save_dict_to_txt(filename, data):
+def save_dict_to_txt(filename):
+    sort_all_by_duration()
     with open(filename, 'w', encoding='utf-8') as file: # Открываем файл для записи
         file.write(f'Отчёт {format_day()}\n') # Записываем заголовок с датой
         
         # Проходим по всем процессам
-        for process, info in data.items():
-            file.write(f"Процесс: {process}\n") # Записываем название процесса
-            file.write(f"   Общая длительность: {format_time(info['total_duration'])}\n") # Записываем общую длительность
-            file.write("    Окна:\n") # Заголовок раздела с окнами
-            
-            # Проходим по всем окнам процесса
-            for window, window_info in info['windows'].items():
-                file.write(f"       Окно: {window}\n") # Записываем название окна
-                file.write(f"           Длительность: {format_time(window_info['duration'])}\n") # Записываем длительность окна
-            
+        for section, info in process_dict.items():
+            file.write(f'Раздел: {section}\n')
+            file.write(f'Общая длительность процессов раздела: {format_time(info['total_duration'])}\n')
+            file.write(f'(процессы: {", ".join(info["processes"].keys())})\n')
+            file.write("\n")
+
+            for process, process_info in info['processes'].items():
+                
+                file.write(f'   Процесс: {process}\n')
+                file.write(f'   Общая длительность процесса: {format_time(process_info['total_duration'])}\n') # Записываем название процесса
+                file.write(f'   (окна: {', '.join(process_info['windows'].keys())})\n')
+                file.write("\n")
+
+                # Проходим по всем окнам процесса
+                for window, window_info in process_info['windows'].items():
+                    file.write(f'       Окно: {window}\n') # Записываем название окна
+                    file.write(f'       Длительность: {format_time(window_info['duration'])}\n') # Записываем длительность окна
+                    file.write("\n")
+  
             file.write("\n") # Добавляем пустую строку между процессами для читаемости
+
+
--- main.py
+++ main.py
@@ -14,19 +14,19 @@ from functions import (
     custom_format,
     format_time,
     format_day,
-    process_dict,
+    get_config_info,
     start_time,
     current_window
 )
 
-# Функция-обработчик, которая будет вызвана при выходе из программы
+""" # Функция-обработчик, которая будет вызвана при выходе из программы
 def exit_handler():
     global start_time, current_window, current_process
     if start_time:
         end_time = time.time()
         create_time_based_report(start_time, end_time, current_window)
-        create_process_dict(current_process, current_window, start_time, end_time)
-        save_dict_to_txt(f'activity_report_{custom_format()}.txt', process_dict)
+        create_process_dict(current_process, current_window, current_path, start_time, end_time)
+        save_dict_to_txt(f'activity_report_{custom_format()}.txt')
 
     print("Отчёты обновлены")
     
@@ -36,32 +36,64 @@ def exit_handler():
 # Регистрируем функцию exit_handler как обработчик выхода
 # Она будет автоматически вызвана при завершении программы
 atexit.register(exit_handler)
+ """
 
+# Файл для конфигов
+config_file = 'config.txt'
 
-#  Создаём файл для повременного отчёта
-with open(f'time_based_report_{custom_format()}.txt', 'w', encoding='utf-8') as file:
-    file.write(f'Отчёт {format_day()}\n')
+# Получение конфигов
+config_info = get_config_info(config_file)
 
-#  Создаём файл для отчёта активности
-with open(f'activity_report_{custom_format()}.txt', 'w', encoding='utf-8') as file:
-    file.write(f'Отчёт {format_day()}\n')
+# Лог-файлы
+time_based_report = f'time_based_report_{custom_format()}.txt'
+activity_report = f'activity_report_{custom_format()}.txt'
+
+# Фиксирование сегодняшней даты
+day = format_day()
+
+# Создаём файл для повременного отчёта
+with open(f'{time_based_report}', 'a', encoding='utf-8') as file:
+    file.write(f'Отчёт {day}\n')
+
+# Создаём файл для отчёта активности
+with open(f'{activity_report}', 'a', encoding='utf-8') as file:
+    file.write(f'Отчёт {day}\n')
 
 # Техническая писанина
-print(f'Программа запущена. Дата: {format_day()}')
-print(f'Создан файл time_based_report_{custom_format()}.txt')
-print(f'Создан файл activity_report_{custom_format()}.txt.txt')
+print(
+    f'Программа запущена. Дата: {day}',
+    f'Создан файл {time_based_report}',
+    f'Создан файл {activity_report}',
+    sep='\n'
+)
 
 try:
     # Бесконечный цикл для постоянного отслеживания активности
+    print(f'Идёт отслеживание процессов')
     while True:
+
         # Получаем информацию об активном окне
         active_window_info = get_active_window_info()
         active_window_name = active_window_info['window_title']
         active_process_name = active_window_info['process_name']
-        check_activity = resumption_activity()
+        active_process_path = active_window_info['process_path']
+        check_activity = resumption_activity(config_info)
 
+        # Обработка состояния AFK (Away From Keyboard)
+        if check_activity:
+            # Записываем время AFK в файл
+            with open(f'time_based_report_{custom_format()}.txt', 'a', encoding='utf-8') as file:
+                file.write(f'{format_time(check_activity[0])}-{format_time(check_activity[1])} : Время AFK\n')
+            
+            afk_start, afk_end = check_activity
+
+            # Формируем запись AFK в словарь процессов
+            create_process_dict('AFK', 'AFK', 'AFK', afk_start, afk_end)
+            # Сохраняем данные словаря
+            save_dict_to_txt(f'activity_report_{custom_format()}.txt')
+            
         # Если текущее окно еще не определено
-        if current_window is None:
+        elif current_window is None:
             start_time = time.time() # Запоминаем начальное время
 
         # Если активное окно изменилось    
@@ -71,32 +103,22 @@ try:
 
                 # Делаем записи в отчёты/словари
                 create_time_based_report(start_time, end_time, current_window)
-                create_process_dict(current_process, current_window, start_time, end_time)
+                create_process_dict(current_process, current_window, current_path, start_time, end_time)
                 
                 # Сохраняем словарь в файл
-                save_dict_to_txt(f'activity_report_{custom_format()}.txt', process_dict)
+                save_dict_to_txt(f'activity_report_{custom_format()}.txt')
                 
                 start_time = time.time() # Обновляем начальное время
         
 
-        # Обработка состояния AFK (Away From Keyboard)
-        elif check_activity:
-            # Записываем время AFK в файл
-            with open(f'time_based_report_{custom_format()}.txt', 'a', encoding='utf-8') as file:
-                file.write(f'{format_time(check_activity[0])}-{format_time(check_activity[1])} : Время AFK\n')
-            
-            afk_start, afk_end = check_activity
 
-            # Формируем запись AFK в словарь процессов
-            create_process_dict('AFK', 'AFK', afk_start, afk_end)
-            # Сохраняем данные словаря
-            save_dict_to_txt(f'activity_report_{custom_format()}.txt', process_dict)
 
         # Обновляем текущие значения
         current_window = active_window_name
         current_process = active_process_name
+        current_path = active_process_path
 
-        time.sleep(5)
+        time.sleep(config_info['time_check_sec'])
 
 except KeyboardInterrupt: 
     print("\nПрограмма остановлена пользователем")
